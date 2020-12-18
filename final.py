29ea5b81a472ccfd6a4de2ec0747a4e6a73e7492
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-12-17 18:42:45 -0800

    finished structured-english

---
+++ final.py
@@ -0,0 +1,11 @@
+"""
+The program is trying to display the number of grades, the average grade,
+and the percent of grades that are above the average grade using the grades found in the final.txt file.
+The number of grades are 24
+The average grade is 83.25.
+The percentage of grades above average are 54.17%. 
+
+The first two functions will display the number of grades and the average grade.
+There will be a function that will be used in order to display the percentage of grades above
+the average grade of 54.17%.
+"""
